75cef6e37efb407cf31d6bafd7ba1bc5190fca51
Poornima sharma <shikhapoornima@gmail.com>
2025-05-20 23:07:50 +0530

    Update:15/05

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -3,9 +3,10 @@ from base.views import order_views as views
 from base.views.order_views import user_shipping_address  
 
 urlpatterns=[
-    
+    path('', views.getOrders, name='orders'),
     path('add/', views.addOrderItems, name='orders-add'),
     path('myorders/', views.getMyOrders, name='myorders'),
+    path('<str:pk>/deliver/', views.updateOrderToDelivered, name='order-delivered'),
     path('<str:pk>/', views.getOrderById, name='user-order'),
     path('<str:pk>/pay/', views.updateOrderToPaid, name='pay'),
     path('api/user/shipping/', user_shipping_address, name='user-shipping-address'),
