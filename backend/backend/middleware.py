32f18713029c5514b0e9e315ea0cddd39368e7a4
Poornima Sharma <129762914+poornimash05@users.noreply.github.com>
2025-07-07 16:17:05 +0530

    Update middleware.py

--- backend/backend/middleware.py
+++ backend/backend/middleware.py
@@ -2,9 +2,11 @@
 
 from django.utils.deprecation import MiddlewareMixin
 from django.conf import settings
+from django.http import HttpResponse
 
 class CORSMediaMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         if request.path.startswith(settings.MEDIA_URL):
-            response["Access-Control-Allow-Origin"] = "*"
+            response['Access-Control-Allow-Origin'] = 'https://bookstore-testing.netlify.app'
+            response['Access-Control-Allow-Credentials'] = 'true'
         return response
