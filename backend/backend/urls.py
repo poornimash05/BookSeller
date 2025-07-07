0af38a1c69558c9360e040a657d0a1c66be657f5
Poornima Sharma <129762914+poornimash05@users.noreply.github.com>
2025-07-07 14:36:37 +0530

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
