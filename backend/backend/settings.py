dc5f689a13361a67aaf83738a21e655919f8d2d1
Poornima Sharma <129762914+poornimash05@users.noreply.github.com>
2025-07-07 15:47:50 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -171,7 +171,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True  # ✅ Or restrict to specific frontend
+
 CORS_ALLOWED_ORIGINS = [
     "https://bookstore-testing.netlify.app",
 ]
