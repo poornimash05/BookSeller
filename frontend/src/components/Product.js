c23775f95bda4244fa5237596342470e11c455a4
Poornima Sharma <129762914+poornimash05@users.noreply.github.com>
2025-07-07 14:57:53 +0530

    Update Product.js

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -12,7 +12,7 @@ function Product({ product }) {
     <Card className="my-3 p-3 rounded shadow-sm border-0 book-card h-100">
       <Link to={`/product/${product._id}`}>
         <Card.Img
-          src={`${BACKEND_URL}/media/${product.image}`}   {/* ✅ updated path */}
+          src={`${BACKEND_URL}/media/${product.image}`}
           variant="top"
           className="book-img"
           alt={product.name}
