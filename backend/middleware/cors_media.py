da8c14553697d296a249f8f6a784517b9166901d
Poornima Sharma <129762914+poornimash05@users.noreply.github.com>
2025-07-07 15:49:45 +0530

    Create cors_media.py

---
+++ backend/middleware/cors_media.py
@@ -0,0 +1,9 @@
+# backend/middleware/cors_media.py
+from django.utils.deprecation import MiddlewareMixin
+from django.conf import settings
+
+class CORSMediaMiddleware(MiddlewareMixin):
+    def process_response(self, request, response):
+        if request.path.startswith(settings.MEDIA_URL):
+            response["Access-Control-Allow-Origin"] = "*"
+        return response
